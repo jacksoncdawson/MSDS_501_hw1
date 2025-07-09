1cb87995fe339a5b1733b67c453af6c55e98dcbc
Jack Dawson <150480928+jacksoncdawson@users.noreply.github.com>
2025-07-09 16:51:21 -0700

    add actual hw1

--- hw1.sh
+++ hw1.sh
@@ -1,7 +1,36 @@
-if [ $# -ne 2 ]
-	then 
-	echo "Usage: sh hw1.sh git_url folder_path"; exit 1
+#!/bin/bash
+
+# read parameters: 1st git url, 2nd local directory
+GIT_URL="$1"
+DIR_NAME="$2"
+
+# two parameters
+if [ -z "$GIT_URL" ] || [ -z "$DIR_NAME" ]; then
+    echo "Please check the format."
+    echo "Example: \"sh ./hw1.sh git_url folder_path\""
+    exit 1 
 fi
 
-echo "Correct Usage!"
-exit 0
+# check for clone
+if [ ! -d "$DIR_NAME" ]; then
+    if git clone "$GIT_URL" "$DIR_NAME"; then
+        echo "Success: Cloned from $GIT_URL to $DIR_NAME"
+        ls -l $DIR_NAME
+        exit 0
+    else
+        echo "Failed to clone from $GIT_URL to $DIR_NAME"
+        exit 1
+    fi
+# check for pull
+else
+    cd $DIR_NAME
+
+    if git pull; then
+        echo "Success: Pulled from $GIT_URL to $DIR_NAME"
+        ls -l
+        exit 0
+    else
+        echo "Failed to pull from $GIT_URL to $DIR_NAME"
+        exit 1
+    fi
+fi
